5a2a1b4af2bcc50ddeb3c63798646e2c0afea032
sandipan-das-sd <sd901656@gmail.com>
2026-05-18 00:41:09 +0530

    feat:Global exception added

--- taskmanager/src/main/java/com/taskapp/taskmanager/exception/GlobalExceptionHandler.java
+++ taskmanager/src/main/java/com/taskapp/taskmanager/exception/GlobalExceptionHandler.java
@@ -1,4 +1,32 @@
 package com.taskapp.taskmanager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", ex.getMessage());
+        return error;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return errors;
+    }
 }
