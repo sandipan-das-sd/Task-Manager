68875633ccae6064955a12f0563c7cbc0a3e912f
sandipan-das-sd <sd901656@gmail.com>
2026-05-17 18:21:48 +0530

    feat:All Repositary added

--- taskmanager/src/main/java/com/taskapp/taskmanager/repositary/TaskRepository.java
+++ taskmanager/src/main/java/com/taskapp/taskmanager/repositary/TaskRepository.java
@@ -1,9 +1,16 @@
 package com.taskapp.taskmanager.repositary;
 import com.taskapp.taskmanager.entity.Task;
+import com.taskapp.taskmanager.entity.TaskPriority;
+import com.taskapp.taskmanager.entity.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
+List<Task> findByProjectId(Long id);
+List<Task> findByStatus(TaskStatus status);
+List<Task> findByPriority(TaskPriority priority);
+List<Task> findByAssignedToId(Long assignedToId);
 }
