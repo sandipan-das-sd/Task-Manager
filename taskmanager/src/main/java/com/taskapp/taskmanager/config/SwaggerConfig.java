d7317e67dbbe03ce9560e71c7047f306e15a100a
sandipan-das-sd <sd901656@gmail.com>
2026-05-18 01:06:36 +0530

    feat:Swagger api added for Api documenation

---
+++ taskmanager/src/main/java/com/taskapp/taskmanager/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.taskapp.taskmanager.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenApi()
+    {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task Manager API")
+                        .version("1.0")
+                        .description("Spring Boot Task Manger Backend")
+
+                );
+    }
+}
